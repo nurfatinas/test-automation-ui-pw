fe4feb9d39dc7076eaa505bdd29b566d7acbe1e9
nurfatin.abdullah <nurfatin.abdullah@paynet.my>
2026-03-26 18:56:51 +0800

    update report path

--- config/config.ts
+++ config/config.ts
@@ -2,4 +2,5 @@ export const config = {
   baseURL: process.env.BASE_URL || 'https://sauce-demo.myshopify.com/',
   timeout: 30000,
   screenshotPath: './screenshots/',
+  reportPath: './playwright-report/'
 };
