3d605141cfe5cd3ed4087c93de85422852886c69
nurfatin.abdullah <nurfatin.abdullah@paynet.my>
2026-03-26 18:57:11 +0800

    update take screenshot helper

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -4,8 +4,6 @@ import fs from 'fs';
 import path from 'path';
 
 export async function takeScreenshot(page: Page, filename: string) {
-  const fullPath = path.join(config.screenshotPath, `${filename}.png`);
-  // make folder if it doesn't exist
   fs.mkdirSync(config.screenshotPath, { recursive: true });
-  await page.screenshot({ path: fullPath });
+  await page.screenshot({ path: path.join(config.screenshotPath, `${filename}.png`) });
 }
